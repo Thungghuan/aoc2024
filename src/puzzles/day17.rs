473c2c555ceb4512bc7aa380c2ccd64c944aec57
Thungghuan <1092887426@qq.com>
2024-12-19 23:23:46 +0800

    solve day17 part1

--- src/puzzles/day17.rs
+++ src/puzzles/day17.rs
@@ -8,6 +8,7 @@ struct Input {
     register_c: i32,
 
     program: Program,
+    code: Vec<i32>,
 }
 
 type Program = Vec<Instruction>;
@@ -31,6 +32,81 @@ enum Operand {
     Ignore(i32),
 }
 
+mod parser {
+    use nom::IResult;
+
+    use super::{Input, Instruction, Operand, Program};
+
+    fn parse_program(input: &str) -> IResult<&str, (Vec<i32>, Program)> {
+        let (input, code) = nom::sequence::preceded(
+            nom::bytes::complete::tag("Program: "),
+            nom::multi::separated_list1(
+                nom::bytes::complete::tag(","),
+                nom::character::complete::i32,
+            ),
+        )(input)?;
+
+        Ok((
+            input,
+            (
+                code.clone(),
+                code.chunks(2)
+                    .map(|chunk| match chunk[0] {
+                        0 => Instruction::ADV(Operand::Combo(chunk[1])),
+                        1 => Instruction::BXL(Operand::Literal(chunk[1])),
+                        2 => Instruction::BST(Operand::Combo(chunk[1])),
+                        3 => Instruction::JNZ(Operand::Literal(chunk[1] / 2)),
+                        4 => Instruction::BXC(Operand::Ignore(chunk[1])),
+                        5 => Instruction::OUT(Operand::Combo(chunk[1])),
+                        6 => Instruction::BDV(Operand::Combo(chunk[1])),
+                        7 => Instruction::CDV(Operand::Combo(chunk[1])),
+                        _ => panic!("Invalid code"),
+                    })
+                    .collect(),
+            ),
+        ))
+    }
+
+    fn parse_register<'a>(input: &'a str, name: &'a str) -> IResult<&'a str, i32> {
+        nom::sequence::preceded(
+            nom::bytes::complete::tag(format!("Register {}: ", name).as_str()),
+            nom::character::complete::i32,
+        )(input)
+    }
+
+    fn parse_registers(input: &str) -> IResult<&str, (i32, i32, i32)> {
+        let (input, register_a) = parse_register(input, "A")?;
+        let (input, _) = nom::character::complete::newline(input)?;
+        let (input, register_b) = parse_register(input, "B")?;
+        let (input, _) = nom::character::complete::newline(input)?;
+        let (input, register_c) = parse_register(input, "C")?;
+
+        Ok((input, (register_a, register_b, register_c)))
+    }
+
+    pub fn parse(input: &str) -> IResult<&str, Input> {
+        nom::combinator::map_res(
+            nom::sequence::preceded(
+                nom::multi::many0(nom::character::complete::newline),
+                nom::sequence::separated_pair(
+                    parse_registers,
+                    nom::bytes::complete::tag("\n\n"),
+                    parse_program,
+                ),
+            ),
+            |((register_a, register_b, register_c), (code, program))| {
+                Ok::<Input, &str>(Input {
+                    register_a,
+                    register_b,
+                    register_c,
+                    program,
+                    code,
+                })
+            },
+        )(input)
+    }
+}
+
 #[derive(Debug)]
 struct Computer {
     register_a: i32,
@@ -40,6 +116,8 @@ struct Computer {
     program: Program,
     instruction_pointer: usize,
     outputs: Vec<i32>,
+
+    jumps: (usize, usize),
 }
 
 impl Computer {
@@ -78,10 +156,69 @@ impl Computer {
                 self.outputs.push(self.calc_combo(combo) % 8);
             }
             Instruction::BDV(Operand::Combo(combo)) => {
-                self.register_b /= 2_i32.pow(self.calc_combo(combo) as u32);
+                self.register_b = self.register_a / 2_i32.pow(self.calc_combo(combo) as u32);
             }
             Instruction::CDV(Operand::Combo(combo)) => {
-                self.register_c /= 2_i32.pow(self.calc_combo(combo) as u32);
+                self.register_c = self.register_a / 2_i32.pow(self.calc_combo(combo) as u32);
+            }
+            _ => panic!("Invalid instruction!"),
+        }
+    }
+
+    fn prev_instruection(&mut self) -> Option<Instruction> {
+        if self.instruction_pointer == 0 {
+            None
+        } else {
+            let instrution = self.program[self.instruction_pointer - 1];
+            self.instruction_pointer -= 1;
+
+            if let Instruction::JNZ(_) = instrution {
+                self.jumps.1 = self.instruction_pointer;
+            }
+
+            if !self.outputs.is_empty() && self.instruction_pointer == self.jumps.0 {
+                self.instruction_pointer = self.jumps.1 + 1;
+            }
+
+            Some(instrution)
+        }
+    }
+
+    fn inverse_instruction(&mut self, instruction: Instruction) {
+        match instruction {
+            Instruction::ADV(Operand::Combo(combo)) => {
+                self.register_a *= 2_i32.pow(self.calc_combo(combo) as u32);
+            }
+            Instruction::BXL(Operand::Literal(literal)) => {
+                self.register_b ^= literal;
+            }
+            Instruction::BST(Operand::Combo(combo)) => match combo {
+                4 => self.register_a = self.register_b,
+                5 => self.register_b = self.register_b,
+                6 => self.register_c = self.register_b,
+                _ => (),
+            },
+            Instruction::JNZ(Operand::Literal(literal)) => {
+                self.jumps.0 = literal as usize;
+            }
+            Instruction::BXC(Operand::Ignore(_)) => {
+                self.register_b ^= self.register_c;
+            }
+            Instruction::OUT(Operand::Combo(combo)) => {
+                if let Some(output) = self.outputs.pop() {
+                    match combo {
+                        4 => self.register_a += output - (self.register_a % 8),
+                        5 => self.register_b += output - (self.register_b % 8),
+                        6 => self.register_c += output - (self.register_c % 8),
+                        _ => (),
+                    }
+                }
+            }
+            Instruction::BDV(Operand::Combo(_combo)) => {
+                self.register_b = 1;
+            }
+            Instruction::CDV(Operand::Combo(_combo)) => {
+                self.register_c = 1;
             }
             _ => panic!("Invalid instruction!"),
         }
@@ -95,77 +232,6 @@ impl Computer {
             6 => self.register_c,
             _ => panic!("Invalid combo operand!"),
         }
-    }
-}
-
-mod parser {
-    use nom::IResult;
-
-    use super::{Input, Instruction, Operand, Program};
-
-    fn parse_program(input: &str) -> IResult<&str, Program> {
-        let (input, code) = nom::sequence::preceded(
-            nom::bytes::complete::tag("Program: "),
-            nom::multi::separated_list1(
-                nom::bytes::complete::tag(","),
-                nom::character::complete::i32,
-            ),
-        )(input)?;
-
-        Ok((
-            input,
-            code.chunks(2)
-                .map(|chunk| match chunk[0] {
-                    0 => Instruction::ADV(Operand::Combo(chunk[1])),
-                    1 => Instruction::BXL(Operand::Literal(chunk[1])),
-                    2 => Instruction::BST(Operand::Combo(chunk[1])),
-                    3 => Instruction::JNZ(Operand::Literal(chunk[1] / 2)),
-                    4 => Instruction::BXC(Operand::Ignore(chunk[1])),
-                    5 => Instruction::OUT(Operand::Combo(chunk[1])),
-                    6 => Instruction::BDV(Operand::Combo(chunk[1])),
-                    7 => Instruction::CDV(Operand::Combo(chunk[1])),
-                    _ => panic!("Invalid code"),
-                })
-                .collect(),
-        ))
-    }
-
-    fn parse_register<'a>(input: &'a str, name: &'a str) -> IResult<&'a str, i32> {
-        nom::sequence::preceded(
-            nom::bytes::complete::tag(format!("Register {}: ", name).as_str()),
-            nom::character::complete::i32,
-        )(input)
-    }
-
-    fn parse_registers(input: &str) -> IResult<&str, (i32, i32, i32)> {
-        let (input, register_a) = parse_register(input, "A")?;
-        let (input, _) = nom::character::complete::newline(input)?;
-        let (input, register_b) = parse_register(input, "B")?;
-        let (input, _) = nom::character::complete::newline(input)?;
-        let (input, register_c) = parse_register(input, "C")?;
-
-        Ok((input, (register_a, register_b, register_c)))
-    }
-
-    pub fn parse(input: &str) -> IResult<&str, Input> {
-        nom::combinator::map_res(
-            nom::sequence::preceded(
-                nom::multi::many0(nom::character::complete::newline),
-                nom::sequence::separated_pair(
-                    parse_registers,
-                    nom::bytes::complete::tag("\n\n"),
-                    parse_program,
-                ),
-            ),
-            |((register_a, register_b, register_c), program)| {
-                Ok::<Input, &str>(Input {
-                    register_a,
-                    register_b,
-                    register_c,
-                    program,
-                })
-            },
-        )(input)
     }
 }
 
@@ -186,6 +252,7 @@ impl Puzzle for Day17 {
             register_a,
             register_b,
             register_c,
+            code: _,
             program,
         } = input;
 
@@ -196,6 +263,7 @@ impl Puzzle for Day17 {
             program,
             instruction_pointer: 0,
             outputs: Vec::new(),
+            jumps: (0, 0),
         };
 
         while let Some(instruction) = computer.next_instruection() {
@@ -211,7 +279,36 @@ impl Puzzle for Day17 {
     }
 
     fn part2(&self, input: &str) -> Self::Output {
-        Ok("".into())
+        let input = self.parse(input)?;
+        let Input {
+            register_a: _,
+            register_b,
+            register_c,
+            code,
+            program,
+        } = input;
+
+        let instruction_pointer = program.len();
+        let mut computer = Computer {
+            register_a: 0,
+            register_b,
+            register_c,
+            program,
+            instruction_pointer,
+            outputs: code,
+            jumps: (0, 0),
+        };
+
+        while let Some(instruction) = computer.prev_instruection() {
+            computer.inverse_instruction(instruction);
+            println!("{:?}", instruction);
+            println!(
+                "{} {} {}",
+                computer.register_a, computer.register_b, computer.register_c
+            );
+        }
+
+        Ok(format!("{}", computer.register_a))
     }
 
     fn solve(&self, input: &str) {
@@ -233,6 +330,13 @@ Register B: 0
 Register C: 0
 
 Program: 0,1,5,4,3,0";
+
+    const TESTCASE2: &'static str = r"
+Register A: 117440
+Register B: 0
+Register C: 0
+
+Program: 0,3,5,4,3,0";
 
     #[test]
     fn test_puzzle_day17_parse() {
@@ -263,6 +367,7 @@ Program: 0,1,5,4,3,0";
             ],
             instruction_pointer: 0,
             outputs: Vec::new(),
+            jumps: (0, 0),
         };
 
         while let Some(instruction) = computer.next_instruection() {
@@ -286,11 +391,10 @@ Program: 0,1,5,4,3,0";
             register_a: 0,
             register_b: 29,
             register_c: 0,
-            program: vec![
-                Instruction::BXL(Operand::Literal(7)),
-            ],
+            program: vec![Instruction::BXL(Operand::Literal(7))],
             instruction_pointer: 0,
             outputs: Vec::new(),
+            jumps: (0, 0),
         };
 
         while let Some(instruction) = computer.next_instruection() {
@@ -306,11 +410,10 @@ Program: 0,1,5,4,3,0";
             register_a: 0,
             register_b: 2024,
             register_c: 43690,
-            program: vec![
-                Instruction::BXC(Operand::Ignore(0)),
-            ],
+            program: vec![Instruction::BXC(Operand::Ignore(0))],
             instruction_pointer: 0,
             outputs: Vec::new(),
+            jumps: (0, 0),
         };
 
         while let Some(instruction) = computer.next_instruection() {
@@ -328,12 +431,16 @@ Program: 0,1,5,4,3,0";
             puzzle.part1(&TESTCASE).unwrap(),
             String::from("4,6,3,5,6,3,5,2,1,0")
         );
+        assert_eq!(
+            puzzle.part1(&TESTCASE2).unwrap(),
+            String::from("0,3,5,4,3,0")
+        );
     }
 
     #[test]
     fn test_puzzle_day17_part2() {
         let puzzle = Day17;
 
-        assert_eq!(puzzle.part2(&TESTCASE).unwrap(), String::new());
+        assert_eq!(puzzle.part2(&TESTCASE2).unwrap(), String::from("117440"));
     }
 }
